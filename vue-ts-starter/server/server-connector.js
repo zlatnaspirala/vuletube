fc183149591016472aeb85f4b8e9c9806a819460
Nidza <zlatnaspirala@gmail.com>
2020-07-03 18:24:53 +0200

    test

--- vue-ts-starter/server/server-connector.js
+++ vue-ts-starter/server/server-connector.js
@@ -24,12 +24,12 @@ var options = {
 
     request.addListener('end', function() {
 
-      if (request.url.search(/.png|.gif|.js|.css/g) != -1) {
-        file.serveFile('bad.html', 402, {}, request, response);
-      } else {
+      // if (request.url.search(/.png|.gif|.js|.css/g) != -1) {
+        // file.serveFile('bad.html', 402, {}, request, response);
+      // } else {
         console.log("SERVER PUBLIC");
         file.serve(request, response);
-      }
+      // }
 
     }).resume();
 
