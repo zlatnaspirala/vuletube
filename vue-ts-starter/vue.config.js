a76470579182a0e838e3955c78d6f2fea0705a50
Nidza <zlatnaspirala@gmail.com>
2020-07-03 18:07:10 +0200

    update

--- vue-ts-starter/vue.config.js
+++ vue-ts-starter/vue.config.js
@@ -8,10 +8,17 @@ module.exports = {
       chunks: ['chunk-vendors', 'chunk-common', 'index']
     },
     'bad': {
-      entry: './src/main.ts',
+      entry: './src/error-instance.ts',
       template: 'public/bad.html',
       title: 'Error page',
       chunks: ['chunk-vendors', 'chunk-common', 'index']
-    }
+    },
+    'googleVerify': {
+      entry: './src/error-instance.ts',
+      template: 'public/google1813c8928f8cf22f.html',
+      title: 'Error page',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+
   }
 }
