93a119092f17a4811790b3dd9f04191db656d688
Nidza <zlatnaspirala@gmail.com>
2020-07-03 17:38:40 +0200

    Update server-connector.js

--- vue-ts-starter/server/server-connector.js
+++ vue-ts-starter/server/server-connector.js
@@ -12,46 +12,7 @@ var app = express();
 var compression = require("compression");
 const static = require('node-static');
 var file = new (static.Server)('/var/www/html/applications/vue-project/vue-typescript-starter/vue-ts-starter/dist/');
-
 var basePath = '/var/www/html/applications/vue-project/vue-typescript-starter/vue-ts-starter/dist/';
-// express.compress()
-app.use(compression({
-  filter: function () { return true; }
-}));
-
-// Server configuration
-// app.use(express.static(__dirname + "../dist-test")); // Set the static files location
-// app.use(cors());
-
-app.get("*", (req, res) => {
-
-   if(req.secure) {
-    // OK, continue
-    console.log("I got request from client ..SECURE !!.", req);
-  };
-
-  console.log("I got request from client ...", req);
-  // Test native cors,
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Accept-Encoding', 'deflate, gzip;q=1.0, *;q=0.5');
-  res.header('Cache-Control', 'public, max-age=31557600');
-
-  // res.redirect('https://' + req.headers.host + req.url);
-  res.status(200).json({ message: "ok" });
-
-});
-
-// app.use(compression({ filter: shouldCompress }));
-function shouldCompress(req, res) {
-  if (req.headers["x-no-compression"]) {
-    // don't compress responses with this request header
-    return false;
-  }
-
-  // fallback to standard filter function
-  return compression.filter(req, res);
-}
-
 
 var options = {
   key: fs.readFileSync("/etc/httpd/conf/ssl/maximumroulette.com.key"),
