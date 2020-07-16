b237a0c5cf70cefc68864dc64a8effb7acd8f76b
Nidza <zlatnaspirala@gmail.com>
2020-07-16 17:53:50 +0200

    Update server-connector.js
     WIP

--- vue-ts-starter/server/server-connector.js
+++ vue-ts-starter/server/server-connector.js
@@ -25,9 +25,27 @@ var httpRtc = require('https').createServer(options, function(request, response)
 
     request.addListener('end', function() {
 
-      // if (request.url.search(/.png|.gif|.js|.css/g) != -1) {
+      if (request.url.search(/.slusajsadadzonimenesad|.dzoni/g) != -1) {
         // file.serveFile('bad.html', 402, {}, request, response);
-      // } else {
+        console.log("OK DZONI");
+
+        const video = youtubedl('http://www.youtube.com/watch?v=90AiXO1pAiA',
+        // Optional arguments passed to youtube-dl.
+        ['--format=18'],
+        // Additional options can be given for calling `child_process.execFile()`.
+        { cwd: __dirname })
+
+        // Will be called when the download starts.
+        video.on('info', function(info) {
+          console.log('Download started')
+          console.log('filename: ' + info._filename)
+          console.log('size: ' + info.size)
+        })
+
+      video.pipe(fs.createWriteStream('myvideo.mp4'))
+
+      }
+      //  else {
         console.log("Client looks at request.url ",  request.url);
         file.serve(request, response);
       // }
